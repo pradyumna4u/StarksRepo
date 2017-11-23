b9348a75c275a44555065a85920e8c6d2d147f7d
Jagan <jagan.kornana@outlook.com>
2017-11-23 19:11:10 +0530

    [Jagan] Adding message sender and receiver

---
+++ src/main/java/com/api/configuration/JMSConfiguration.java
@@ -0,0 +1,64 @@
+package com.api.configuration;
+
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.QueueConnectionFactory;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.connection.UserCredentialsConnectionFactoryAdapter;
+import org.springframework.jms.core.JmsTemplate;
+
+import com.gap.foundation.configuration.EncryptedString;
+import com.ibm.mq.jms.MQQueueConnectionFactory;
+import com.ibm.msg.client.wmq.WMQConstants;
+
+@Configuration
+public class JMSConfiguration {
+
+    @Value("${ibm.mq.hostname}")
+    private String hostName;
+
+    @Value("${ibm.mq.port}")
+    private int port;
+
+    @Value("${ibm.mq.mgr}")
+    private String manager;
+
+    @Value("${ibm.mq.channel}")
+    private String channel;
+
+    @Value("${ibm.mq.username}")
+    private String username;
+
+    @Value("${ibm.mq.password}")
+    private EncryptedString password;
+
+    @Bean
+    public MQQueueConnectionFactory mqConnectionFactory() throws JMSException {
+        MQQueueConnectionFactory mqQueueConnectionFactory = new MQQueueConnectionFactory();
+        mqQueueConnectionFactory.setHostName(hostName);
+        mqQueueConnectionFactory.setPort(port);
+        mqQueueConnectionFactory.setQueueManager(manager);
+        mqQueueConnectionFactory.setChannel(channel);
+        mqQueueConnectionFactory.setTransportType(WMQConstants.WMQ_CM_CLIENT);
+        return mqQueueConnectionFactory;
+    }
+
+    @Bean
+    public QueueConnectionFactory connectionFactory() throws JMSException {
+        UserCredentialsConnectionFactoryAdapter userCredentialsConnectionFactoryAdapter = new UserCredentialsConnectionFactoryAdapter();
+        userCredentialsConnectionFactoryAdapter
+                .setTargetConnectionFactory(mqConnectionFactory());
+        userCredentialsConnectionFactoryAdapter.setUsername(username);
+        userCredentialsConnectionFactoryAdapter.setPassword(password.decrypt());
+        userCredentialsConnectionFactoryAdapter.createQueueConnection();
+        return userCredentialsConnectionFactoryAdapter;
+    }
+
+    @Bean
+    public JmsTemplate jmsTemplate(ConnectionFactory connectionFactory) {
+        return new JmsTemplate(connectionFactory);
+    }
+}
---
+++ src/main/java/com/api/jms/MessageReceiver.java
@@ -0,0 +1,32 @@
+package com.api.jms;
+
+import javax.jms.JMSException;
+import javax.jms.TextMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageReceiver {
+
+    @Autowired
+    private JmsTemplate jmsTemplate;
+
+    @Value("${ibm.mq.inputQueueName}")
+    private String inputQueue;
+
+    public JmsTemplate getJmsTemplate() {
+        return jmsTemplate;
+    }
+
+    public void setJmsTemplate(JmsTemplate jmsTemplate) {
+        this.jmsTemplate = jmsTemplate;
+    }
+
+    public String receiveMessage() throws JMSException {
+        TextMessage textMessage = (TextMessage) jmsTemplate.receive(inputQueue);
+        return textMessage.getText();
+    }
+}
